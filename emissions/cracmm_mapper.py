c20b815a911c5df4c11e4987686c63e2eedc10e3
Havala Pye <pye.havala@epa.gov>
2021-07-16 17:41:00 -0400

    FURAN, PROG, aromatic, and other mapping updates
    Mapper updates:
    -Added all silanes and siloxanes to ROCIOXY (oxygenated IVOCS)
    -Added furans as FURAN (detected by RDKIT)
    -Added UNKCRACMM mappings for elemental carbon and any non-carbon-containing species
    -Moved glycoaldehyde from ALD to GLY to better reflect its solubility
    -Added explicit propylene glycol (and other 3 carbon dialcohols-none of which exist in our inventory)
    -Eliminated PAH category due to small emissions and little overall OA potential
    -Added polycyclic aromatics to NAPH. NAPH is still dominated by naphthalenes
    -Moved L/SVOC ROC categories before the single-ring aromatic mappings
    -Eliminated IVOCP4ARO (ROCP4ARO) bin due to insufficient emitted mass
    -Eliminated DIEN due to most mass in the category being mapped to FURAN or BDE13
    -ROCIOXY created for oxygenated IVOCs and Si-containing species of any C*
    -All S/IVOCs officially named ROC
    -mapper now synced with other output files in repo
    
    Mapping updates in SPECIATE/mech4import:
    -DIEN and some XYM, ROCP6ARO species now FURANs
    -ROXOXY renamed ROCIOXY
    -some PAHs now ROCP2/1/0/5ARO
    -siloxanes/silanes now ROCIOXY instead of HC3 or HC5
    -TC, EC, now UNKCRACMM instead of SLOWROC
    -propylene glycol moved from ROCOXY to PROG
    
    Representative compound updates
    -Organic carbon and NCOM representative compound updated to Triacontane to get correct mapping based on volatility

--- emissions/cracmm_mapper.py
+++ emissions/cracmm_mapper.py
@@ -22,7 +22,7 @@ def get_cracmm_roc(smiles,koh,log10cstar):
   kOH and C* may not be used if the compound can be mapped without it. 
   RACM2 reference: https://ars.els-cdn.com/content/image/1-s2.0-S1352231012011065-mmc1.pdf
   '''
-  # CRACMMv0.11
+  # CRACMM
   
   # Prep inputs
   m       = Chem.MolFromSmiles(smiles)
@@ -45,6 +45,7 @@ def get_cracmm_roc(smiles,koh,log10cstar):
   nbenzene  = rdkit.Chem.Fragments.fr_benzene(m,countUnique=True)
   nalcohol  = rdkit.Chem.Fragments.fr_Al_OH(m,countUnique=True) + \
               rdkit.Chem.Fragments.fr_Ar_OH(m,countUnique=True)      # aliphatic and aromatic
+  nfuran    = rdkit.Chem.Fragments.fr_furan(m,countUnique=True) # number of furan rings
   for atom in range(len(m.GetAtoms())):
       if m.GetAtomWithIdx(atom).IsInRing(): 
           gotring = 1 # 0 = no ring, 1 = ring
@@ -53,10 +54,14 @@ def get_cracmm_roc(smiles,koh,log10cstar):
   nnitrate = smiles.count('O[N+](=O)[O-]') + smiles.count('O[N+]([O-])=O') 
   nperoxide = smiles.count('COO') +  smiles.count('OOC') - smiles.count('COOC')
 #  namine = smiles.count('CN') + smiles.count('NC') + smiles.count('C(N') + smiles.count('N(C')                                           
-  tfmonoterpene = (nC == 10 and nH == 18 and nO == 1) or (nC == 10 and nH == 16)
-    
+  tfmonoterpene = (nC == 10 and nH == 18 and nO == 1) or (nC == 10 and nH == 16) 
+  
+  # Mapper is for ROC only and not elemental carbon
+  if   ( nC <= 0 ):                 mechspecies = 'UNKCRACMM'
+  elif ( smiles == '[C]' ):         mechspecies = 'UNKCRACMM'
+  
   # Explicit species
-  if   ( smiles == 'CC=O' ):        mechspecies = 'ACD'   # acetaldehyde
+  elif ( smiles == 'CC=O' ):        mechspecies = 'ACD'   # acetaldehyde
   elif ( smiles == 'C#C' ):         mechspecies = 'ACE'   # acetylene
   elif ( smiles == 'CC(=O)C' or smiles == 'CC(C)=O' ): 
                                     mechspecies = 'ACT'   # acetone
@@ -68,8 +73,7 @@ def get_cracmm_roc(smiles,koh,log10cstar):
   elif ( smiles == 'C(CO)O' or smiles == 'OCCO'): 
                                     mechspecies = 'ETEG'  # ethylene glycol  
   elif ( smiles == "CC" ):          mechspecies = 'ETH'   # ethane  
-  elif ( smiles == 'C(=O)C=O' or smiles == 'O=CC=O' ):     
-                                    mechspecies = 'GLY'   # glyoxal
+
   elif ( smiles == 'C=O'):          mechspecies = 'HCHO'  # formaldehyde  
   elif ( smiles == 'CC(=C)C=C' ):   mechspecies = 'ISO'   # isoprene (canonical SMILES)  
   elif ( smiles == 'CO'):           mechspecies = 'MOH'   # methanol   
@@ -80,13 +84,19 @@ def get_cracmm_roc(smiles,koh,log10cstar):
                                     mechspecies = 'MEK'  # methyl ethyl ketone (1.2e-12) 
   elif ( nC==4 and nH==6 and nO==1 and nketone==1 ): 
                                     mechspecies = 'MVK'  # methyl vinyl ketone (2.0e-11)
-                                    
+                                                                                                      
   # Explicit toxics added (new to v0.1)                                 
   elif ( nC==7 and nH==8 and nO==0 and nN==0 and nbenzene==1): 
                                     mechspecies = 'TOLUENE'  # toluene explicit
   elif ( smiles == 'C=CC=C' ):      mechspecies = 'BDE13'    # 1,3 butadiene   
   elif ( smiles == 'C=CC=O' or smiles == 'O=CC=C'):      
                                     mechspecies = 'ACRO' # acrolein                                  
+
+  # Glyoxal and glycoaldehyde (here due to solubility alt mappings: ACD, ETEG)
+  elif ( nC == 2 and nO==2 and naldehyde >= 1 ):     
+                                    mechspecies = 'GLY'   # glyoxal
+  # Propylene glycol
+  elif ( nC == 3 and nO==2 and nalcohol == 2): mechspecies = 'PROG' # propylene glycol and other 3 carbon dialcohols                                
 
   # Low reactivity species (new to v0.1)
   elif ( koh < 3.5e-13 ):                mechspecies = 'SLOWROC' # low reactivity gas    
@@ -97,10 +107,20 @@ def get_cracmm_roc(smiles,koh,log10cstar):
   elif ( nC == 15 and nH == 24 ) and ( nCdblC>=1 ): mechspecies = 'SESQ' # sesquiterpenes
   elif ( tfmonoterpene and nCdblC==1 ): mechspecies = 'API' # a-pinene monoterpenes
   elif ( tfmonoterpene and nCdblC>=2 ): mechspecies = 'LIM' # limonene monoterpenes
+  
+  # Furans and dienes other than 1,3 BDE
+  elif ( nfuran > 0 ):                 mechspecies = 'FURAN' # furans and other dienes
     
   # Multi-ring aromatics (PAH and NAPH can be collapsed together if necessary)  
-  elif ( nbenzene >= 1 and log10cstar < 3.5 ): mechspecies = 'PAH'  # PAH and other lower-volatility aromatics (v0.1)
-  elif ( nbenzene > 1 ):               mechspecies = 'NAPH' # Naphthalene-like, PAH with 2 rings
+  # elif ( nbenzene >= 1 and log10cstar < 3.5 and (nO/nC) == 0 ): mechspecies = 'PAH'  # PAH and other lower-volatility aromatics (v0.1)
+  elif ( nbenzene > 1 and nO/nC == 0 ):mechspecies = 'NAPH' # Naphthalene-like, PAH with 2 rings
+
+  # SVOC species binned
+  elif ( log10cstar < -1.5 ):        mechspecies = 'ROCN2' # C* bin centered on 0.01 ug/m3 
+  elif ( log10cstar < -0.5 ):        mechspecies = 'ROCN1' # C* bin centered on 0.1 ug/m3
+  elif ( log10cstar < 0.5 ):         mechspecies = 'ROCP0' # C* bin centered on 1
+  elif ( log10cstar < 1.5 ):         mechspecies = 'ROCP1' # C* bin centered on 10^1
+  elif ( log10cstar < 2.5 ):         mechspecies = 'ROCP2' # C* bin centered on 10^2
     
   # Single-ring aromatics
   elif ( nbenzene > 0 ): # Single-ring aromatics
@@ -108,23 +128,15 @@ def get_cracmm_roc(smiles,koh,log10cstar):
     elif ( nC>=7 and nalcohol>=2 ):    mechspecies = 'MCT'  # methylcatechol
     elif ( nC>=7 and nalcohol>=1 ):    mechspecies = 'CSL'  # cresol
     elif ( nC==6 and nalcohol>=1 ):    mechspecies = 'PHEN' # phenol
-    elif ( log10cstar < 4.5 ):         mechspecies = 'IVOCP4ARO' # C* bin centered on 10^4 (v0.1)
-    elif ( log10cstar < 5.5 ):         mechspecies = 'IVOCP5ARO' # C* bin centered on 10^5 (v0.1)
-    elif ( log10cstar < 6.5 ):         mechspecies = 'IVOCP6ARO' # C* bin centered on 10^6 (v0.1)
+    elif ( log10cstar < 5.5 ):         mechspecies = 'ROCP5ARO' # C* bin centered on 10^5 (v0.1)
+    elif ( log10cstar < 6.5 ):         mechspecies = 'ROCP6ARO' # C* bin centered on 10^6 (v0.1)
     elif ( koh<=(1.02*1.43e-11) ):     mechspecies = 'XYOP'      # o-,p-xylene and less reactive aromatics (v0.1)
     else:                              mechspecies = 'XYM'       # m-xylene and more reactive aromatics (v0.1)
 
-  # SVOC species binned (new to v0.1)
-  elif ( log10cstar < -0.5 ):        mechspecies = 'SVOCN1' # C* bin centered on 0.1 ug/m3
-  elif ( log10cstar < 0.5 ):         mechspecies = 'SVOCP0' # C* bin centered on 1
-  elif ( log10cstar < 1.5 ):         mechspecies = 'SVOCP1' # C* bin centered on 10^1
-  elif ( log10cstar < 2.5 ):         mechspecies = 'SVOCP2' # C* bin centered on 10^2
-
   # Species with double bonds, not aromatic
-  elif ( nCdblC>=1 and log10cstar < 4.5 ): mechspecies = 'IVOCP4ARO' # C* bin centered on 10^4 (v0.1)
-  elif ( nCdblC>=1 and log10cstar < 5.5 ): mechspecies = 'IVOCP5ARO' # C* bin centered on 10^5 (v0.1)
-  elif ( nCdblC>=1 and log10cstar < 6.5 ): mechspecies = 'IVOCP6ARO' # C* bin centered on 10^6 (v0.1) 
-  elif ( nCdblC>=2 ):                      mechspecies = 'DIEN' # diene (nC>=4 gauranteed)
+  elif ( nCdblC>=1 and log10cstar < 5.5 ): mechspecies = 'ROCP5ARO' # C* bin centered on 10^5 (v0.1)
+  elif ( nCdblC>=1 and log10cstar < 6.5 ): mechspecies = 'ROCP6ARO' # C* bin centered on 10^6 (v0.1) 
+  elif ( nCdblC>=2 ):                      mechspecies = 'FURAN' # some additional dienes (nC>=4 gauranteed)
   elif ( nCdblC==1 and ncarbonyl>=2 ):     mechspecies = 'DCB1' # unsaturated dicarbonyls
   elif ( nCdblC==1 and nC==4 and naldehyde==1 ): 
                                            mechspecies = 'MACR' # methacrolein (2.9e-11) (and crotonaldehyde)
@@ -134,12 +146,12 @@ def get_cracmm_roc(smiles,koh,log10cstar):
   elif ( nCdblC==1 ):                      mechspecies = 'OLI'  # internal alkene (7.1e-11)
 
   # IVOC species binned (new to v0.1)
-  elif ( log10cstar < 6.5 and nO/nC >= 0.1 ):  
-                                     mechspecies = 'IVOCOXY' # Oxygenated IVOCs
-  elif ( log10cstar < 3.5 ):         mechspecies = 'IVOCP3' # C* bin centered on 1000 ug/m3
-  elif ( log10cstar < 4.5 ):         mechspecies = 'IVOCP4' # C* bin centered on 10^4
-  elif ( log10cstar < 5.5 ):         mechspecies = 'IVOCP5' # C* bin centered on 10^5
-  elif ( log10cstar < 6.5 ):         mechspecies = 'IVOCP6' # C* bin centered on 10^6
+  elif ( ( log10cstar < 6.5 and nO/nC >= 0.1 ) or nSi > 0 ):  
+                                     mechspecies = 'ROCIOXY' # Oxygenated IVOCs and any silanes/siloxanes
+  elif ( log10cstar < 3.5 ):         mechspecies = 'ROCP3' # C* bin centered on 1000 ug/m3
+  elif ( log10cstar < 4.5 ):         mechspecies = 'ROCP4' # C* bin centered on 10^4
+  elif ( log10cstar < 5.5 ):         mechspecies = 'ROCP5' # C* bin centered on 10^5
+  elif ( log10cstar < 6.5 ):         mechspecies = 'ROCP6' # C* bin centered on 10^6
 
   # Oxygenated species without double bonds (mapped in order of decreasing koh)
   elif ( naldehyde>=1 and nketone>=1 ):mechspecies = 'MGLY' # methylglyoxal and similar like C4H6O2 (1.5e-11)
